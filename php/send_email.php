59c46b158552b15805bbdf5f2bd2b32f74ddc8d1
khusshhhhh <workforkhush8@gmail.com>
2025-01-12 11:10:10 +1030

    Khush Patel32

--- php/send_email.php
+++ php/send_email.php
@@ -1,35 +1,55 @@
 <?php
-if(isset($_REQUEST['action']) && $_REQUEST['action'] == 'sendEmail')
-{
-	$to = 'support@mail.com';
-	$subject = 'Roxine Contact Form';
-	$send_arr = array();	
-	
-	$headers = "MIME-Version: 1.0" . "\r\n";
-	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-	$headers .= "From: <".$_REQUEST['con_email'].">" . "\r\n";
-	$headers .= "Cc: ".$_REQUEST['con_email'] . "\r\n";
-	
-	$message = "First Name : ".$_REQUEST['con_fname']. "<br />";
-	$message .= "Last Name : ".$_REQUEST['con_lname']. "<br />";
-	$message .= "Email : ".$_REQUEST['con_email']. "<br />";
-	$message .= "Phone : ".$_REQUEST['con_phone']. "<br />";
-	$message .= "Message : ".$_REQUEST['con_message']. "<br />";
-	
-	if (mail($to,$subject,$message,$headers) ){
-		
-		$send_arr['response'] = 'success';
-		$send_arr['message'] = 'Your message has been sent.';
-		
-		} else{
-			
-		$send_arr['response'] = 'error';
-		$send_arr['message'] = "You message couldn't be sent. Please try later!";
-			
-			}
-	echo json_encode($send_arr);
-	exit;
-	
+include 'db_connection.php';
+
+// Set timezone
+date_default_timezone_set('Australia/Adelaide');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	// Get form inputs
+	$name = $conn->real_escape_string($_POST['con_fname']);
+	$phone = $conn->real_escape_string($_POST['con_phone']);
+	$email = $conn->real_escape_string($_POST['con_email']);
+	$state = $conn->real_escape_string($_POST['state_selector']);
+	$post_code = $conn->real_escape_string($_POST['post_code']);
+	$service = $conn->real_escape_string($_POST['service_selector']);
+	$message = $conn->real_escape_string($_POST['con_message']);
+	$created_date = date('Y-m-d'); // Store date only
+
+	// Insert into database
+	$sql = "INSERT INTO contact_us (name, phone, email, state, post_code, service, message, created_date) 
+            VALUES ('$name', '$phone', '$email', '$state', '$post_code', '$service', '$message', '$created_date')";
+
+	if ($conn->query($sql) === TRUE) {
+		// Send email notification
+		$to = "info@mrkbfinance.com.au";
+		$subject = "New Contact Form Submission";
+		$body = "You have received a new submission from your contact form:\n\n" .
+			"Name: $name\n" .
+			"Phone: $phone\n" .
+			"Date: $created_date\n" .
+			"Email: $email\n" .
+			"State: $state\n" .
+			"Post Code: $post_code\n" .
+			"Service: $service\n" .
+			"Message: $message\n";
+
+		$headers = "From: no-reply@mrkbfinance.com.au";
+
+		if (mail($to, $subject, $body, $headers)) {
+			echo "Email sent successfully.";
+		} else {
+			echo "Failed to send email.";
+		}
+
+		// Redirect with dialog box
+		echo "<script>
+            alert('Thank you for your submission! We will get back to you shortly.');
+            window.location.href = 'contact.html';
+        </script>";
+	} else {
+		echo "Error: " . $conn->error;
+	}
 }
 
+$conn->close();
 ?>
