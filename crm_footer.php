9349b12c5bf93d8f9179a9fafb96be33e227ce5e
khusshhhhh <workforkhush8@gmail.com>
2025-01-22 09:42:24 +1030

    Khush Patel150

--- crm_footer.php
+++ crm_footer.php
@@ -1,7 +1,7 @@
 <footer class="bg-dark text-white text-center py-3 mt-4">
     <div class="container">
-        <p>&copy; <?= date("Y"); ?> MRKB Finance. All Rights Reserved. Managed by <span style="color: #ff9d00"><a
-                    href="www.prelette.com/designp.html">prelette.com</a></span></p>
+        <p>&copy; <?= date("Y"); ?> MRKB Finance. All Rights Reserved. Managed by <span><a
+                    href="www.prelette.com/designp.html" style="color: #ff9d00">prelette.com</a></span></p>
     </div>
 </footer>
 
--- crm_header.php
+++ crm_header.php
@@ -14,6 +14,7 @@ if (!isset($_SESSION['user_id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MRKB | CRM</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
